5e15c0d7170eb3bc217e6b9a7d116ddf6b1e6289
wenchengchao <wenchengchao@jd.com>
2019-06-24 10:37:06 +0800

    rm <values.h>

--- featureson.h
+++ featureson.h
@@ -42,7 +42,8 @@ covered by the following copyright and permission notice:
 #include <iostream>
 #include <iomanip>
 #include <vector>
-#include <values.h>
+#include <float.h>
+#include <limits.h>
 #include "online.h"
 
 #define MAXNUMHATS 200
--- online.h
+++ online.h
@@ -39,7 +39,8 @@ covered by the following copyright and permission notice:
 #define ONLINE_H
 
 #include <math.h>
-#include <values.h>
+#include <float.h>
+#include <limits.h>
 #include <iostream>
 #include <string>
 #include <vector>
